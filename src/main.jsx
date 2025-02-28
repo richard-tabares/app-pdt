673a89d2fffb567b60dd3a1d63d81f4d699e955e
RichardTB <richardtabaresb@gmail.com>
2025-02-27 20:07:06 -0500

    Redirects Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, HashRouter } from 'react-router'
 import { AppRouter } from './router/AppRouter.jsx'
 import './Styles.css'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-  <BrowserRouter>
+  //</HashRouter>
+  <HashRouter>
     <AppRouter />
-  </BrowserRouter>
-  // </StrictMode>,
+  </HashRouter>
+  // </BrowserRouter>
+  // </StrictMode>
 )
